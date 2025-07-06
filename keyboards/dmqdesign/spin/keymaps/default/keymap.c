60efe02a54350de3d36fd4ff1c5dc6ca8589a369
brndntylr <72316048+brndntylr@users.noreply.github.com>
2025-07-06 11:08:15 +0100

    Adding Layer Support and default layout

--- keyboards/dmqdesign/spin/keymaps/default/keymap.c
+++ keyboards/dmqdesign/spin/keymaps/default/keymap.c
@@ -15,8 +15,26 @@
  */
 #include QMK_KEYBOARD_H
 
+enum layer_names {
+    _QWERTY,
+    _FN,
+    _ADJ
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(/* Base */
+    [_QWERTY] = LAYOUT(/* Base */
+                KC_7, KC_8, KC_9, KC_TRNS,
+                KC_4, KC_5, KC_6, KC_TRNS,
+                KC_1, KC_2, KC_3, KC_TRNS,
+                KC_0, UG_TOGG, KC_ENTER
+                ),
+    [_FN] = LAYOUT(/* Layer 1 */
+                KC_7, KC_8, KC_9, KC_TRNS,
+                KC_4, KC_5, KC_6, KC_TRNS,
+                KC_1, KC_2, KC_3, KC_TRNS,
+                KC_0, UG_TOGG, KC_ENTER
+                ),
+    [_ADJ] = LAYOUT(/* Layer 2 */
                 KC_7, KC_8, KC_9, KC_TRNS,
                 KC_4, KC_5, KC_6, KC_TRNS,
                 KC_1, KC_2, KC_3, KC_TRNS,
